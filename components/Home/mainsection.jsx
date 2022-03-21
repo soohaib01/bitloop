5bb030b26baf4292dbce388bdf65f92da9a230f9
sohaib nadeem <sohaibnadeem876@gmail.com>
2022-03-21 21:34:57 +0500

    Last Update

--- components/Home/mainsection.jsx
+++ components/Home/mainsection.jsx
@@ -4,13 +4,13 @@ const mainsection = () => {
   return (
     <div className={MainStyles.TopContainer}>
         <div className={MainStyles.HeadingContainer}>
-        <h1 className={MainStyles.Headingmain}><span className={MainStyles.ColorFull}>Develop</span> backend faster with <span className={MainStyles.ColorFull}>Bitloops</span></h1>
+        <h1 className={MainStyles.Headingmain}><span className={MainStyles.ColorFull}>Develop</span> backend 10X faster with <span className={MainStyles.ColorFull}>Bitloops</span></h1>
         <p className={MainStyles.Content}>Build, scale, extend and manage your backend, picoservices, microservices and APIs with Bitloops</p>
         <button className={MainStyles.BtnOne}>Get Started For Free</button>
         <button className={MainStyles.BesideBtn}>Questions? Talk to an expert</button>
         </div>
         <div className={MainStyles.ImageContainer}>
-            <img src="https://www.netlify.com/v3/img/build/lrOzZEYMmu-650.avif" alt="" />
+            <img src="/Bitloops.gif" alt="" className={MainStyles.Image} />
         </div>
     </div>
   )
