56058eaae7bca3e31a58cdfed42d4ae97fd4a428
sohaib nadeem <sohaibnadeem876@gmail.com>
2022-03-22 12:49:34 +0500

    Update SOme Of the components

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -16,14 +16,12 @@ const navbar = () => {
   return (
     <div className={Nav.Container}>
       <div className={Nav.mainContainer}>
+    
           <img src='https://global-uploads.webflow.com/6183aca39d7d1eea0ba31f1f/6184bd8f4ed3b9dc15f317de_bitloops-logo_320x80.png'
         className={Nav.LogoContain}
         />
         <ul className={Nav.Listing}>
           <li><Link href="/">Home</Link></li>
-         
-
-       
           <li className={Nav.HoverShow} onClick={HandleClick}><Link href="#">React Backend</Link><FontAwesomeIcon
         icon={faChevronDown}
         style={{ fontSize: 12,marginLeft:8,cursor:'pointer', color: "black" }}
@@ -46,8 +44,8 @@ const navbar = () => {
    
       <div className={Nav.RightContainer}>
       <ul className={Nav.Listing}>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/">React</Link></li>
+  
+          <li><Link href="https://github.com/bitloops/bitloops">Source Github</Link></li>
           
           <li><Link href="/">Sign In</Link></li>
         
