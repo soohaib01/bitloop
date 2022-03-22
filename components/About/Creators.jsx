c6cb70af7bbb6075ffd49003eb2d324e8bbc0860
sohaib nadeem <sohaibnadeem876@gmail.com>
2022-03-22 18:45:56 +0500

    Mobile Check

--- components/About/Creators.jsx
+++ components/About/Creators.jsx
@@ -10,22 +10,22 @@ const Creators = () => {
           <div className={Styles.Content}>
               <img src="/iconOne.png" alt="User Led Product" className={Styles.Icon} />
               <h3>User-Led Product</h3>
-              <p>Our product roadmap will be driven by our Users. Help us help you!</p>
+              <p>Our product roadmap will be driven <br></br> by our Users. Help us help you!</p>
           </div>
           <div className={Styles.Content}>
               <img src="/iconTwo.png" alt="User Led Product" className={Styles.Icon} />
               <h3>User-Led Product</h3>
-              <p>Our product roadmap will be driven by our Users. Help us help you!</p>
+              <p>Our product roadmap will be driven <br></br> by our Users. Help us help you!</p>
           </div>
           <div className={Styles.Content}>
               <img src="/iconThree.png" alt="User Led Product" className={Styles.Icon} />
               <h3>User-Led Product</h3>
-              <p>Our product roadmap will be driven by our Users. Help us help you!</p>
+              <p>Our product roadmap will be driven <br></br> by our Users. Help us help you!</p>
           </div>
             <div className={Styles.Content}>
               <img src="/iconFour.png" alt="User Led Product" className={Styles.Icon} />
               <h3>User-Led Product</h3>
-              <p>Our product roadmap will be driven by our Users. Help us help you!</p>
+              <p>Our product roadmap will be driven <br></br> by our Users. Help us help you!</p>
           </div>
         </div>
         </div>
