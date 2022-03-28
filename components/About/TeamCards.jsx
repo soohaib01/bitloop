42924506548f7c123988be48d9c280fb88dc090d
sohaib nadeem <sohaibnadeem876@gmail.com>
2022-03-28 18:26:52 +0500

    Three Pages Completed For Review

--- components/About/TeamCards.jsx
+++ components/About/TeamCards.jsx
@@ -4,7 +4,117 @@ const TeamCards = () => {
   return (
     <div>
         <div className={Styles.Container}>
-            
+            <div className={Styles.Heading}>
+                <h1>Meet the Bitloops team</h1>
+            </div>
+            <div className={Styles.CardContainer}>
+            <div className={Styles.Card}>
+                <img src="/Teams/danias.jpg" alt="Danias" className={Styles.MemberImage} />
+                <h2>Vasilis Danias</h2>
+                <div className={Styles.ForBorder}></div>
+                <div style={{
+                    display:'flex',
+                    justifyContent:'space-between'
+                }}>
+                <p className={Styles.Designation}>Co-Founder & CEO</p>
+                 <img src="/Teams/link.png" alt="Linkdin Image" className={Styles.Icon} />
+                </div>
+                
+            </div>
+            <div className={Styles.Card}>
+                <img src="/Teams/danias.jpg" alt="Danias" className={Styles.MemberImage} />
+                <h2>Vasilis Danias</h2>
+                <div className={Styles.ForBorder}></div>
+                <div style={{
+                    display:'flex',
+                    justifyContent:'space-between'
+                }}>
+                <p className={Styles.Designation}>Co-Founder & CEO</p>
+                 <img src="/Teams/link.png" alt="Linkdin Image" className={Styles.Icon} />
+                </div>
+                
+            </div>
+            <div className={Styles.Card}>
+                <img src="/Teams/danias.jpg" alt="Danias" className={Styles.MemberImage} />
+                <h2>Vasilis Danias</h2>
+                <div className={Styles.ForBorder}></div>
+                <div style={{
+                    display:'flex',
+                    justifyContent:'space-between'
+                }}>
+                <p className={Styles.Designation}>Co-Founder & CEO</p>
+                 <img src="/Teams/link.png" alt="Linkdin Image" className={Styles.Icon} />
+                </div>
+                
+            </div>
+            <div className={Styles.Card}>
+                <img src="/Teams/danias.jpg" alt="Danias" className={Styles.MemberImage} />
+                <h2>Vasilis Danias</h2>
+                <div className={Styles.ForBorder}></div>
+                <div style={{
+                    display:'flex',
+                    justifyContent:'space-between'
+                }}>
+                <p className={Styles.Designation}>Co-Founder & CEO</p>
+                 <img src="/Teams/link.png" alt="Linkdin Image" className={Styles.Icon} />
+                </div>
+                
+            </div>
+            </div>
+            <div className={Styles.CardContainer}>
+            <div className={Styles.Card}>
+                <img src="/Teams/danias.jpg" alt="Danias" className={Styles.MemberImage} />
+                <h2>Vasilis Danias</h2>
+                <div className={Styles.ForBorder}></div>
+                <div style={{
+                    display:'flex',
+                    justifyContent:'space-between'
+                }}>
+                <p className={Styles.Designation}>Co-Founder & CEO</p>
+                 <img src="/Teams/link.png" alt="Linkdin Image" className={Styles.Icon} />
+                </div>
+                
+            </div>
+            <div className={Styles.Card}>
+                <img src="/Teams/danias.jpg" alt="Danias" className={Styles.MemberImage} />
+                <h2>Vasilis Danias</h2>
+                <div className={Styles.ForBorder}></div>
+                <div style={{
+                    display:'flex',
+                    justifyContent:'space-between'
+                }}>
+                <p className={Styles.Designation}>Co-Founder & CEO</p>
+                 <img src="/Teams/link.png" alt="Linkdin Image" className={Styles.Icon} />
+                </div>
+                
+            </div>
+            <div className={Styles.Card}>
+                <img src="/Teams/danias.jpg" alt="Danias" className={Styles.MemberImage} />
+                <h2>Vasilis Danias</h2>
+                <div className={Styles.ForBorder}></div>
+                <div style={{
+                    display:'flex',
+                    justifyContent:'space-between'
+                }}>
+                <p className={Styles.Designation}>Co-Founder & CEO</p>
+                 <img src="/Teams/link.png" alt="Linkdin Image" className={Styles.Icon} />
+                </div>
+                
+            </div>
+            <div className={Styles.Card}>
+                <img src="/Teams/danias.jpg" alt="Danias" className={Styles.MemberImage} />
+                <h2>Vasilis Danias</h2>
+                <div className={Styles.ForBorder}></div>
+                <div style={{
+                    display:'flex',
+                    justifyContent:'space-between'
+                }}>
+                <p className={Styles.Designation}>Co-Founder & CEO</p>
+                 <img src="/Teams/link.png" alt="Linkdin Image" className={Styles.Icon} />
+                </div>
+                
+            </div>
+            </div>
         </div>
     </div>
   )
